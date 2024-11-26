794d11e8ee866e56d8b3ccb0b0c9d08568d8387f
August Sørensen <143783928+augustrs@users.noreply.github.com>
2024-11-26 11:31:46 +0100

    Initial commit

---
+++ src/main/java/com/example/retrogamer/RetroGamerApplication.java
@@ -0,0 +1,13 @@
+package com.example.retrogamer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetroGamerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RetroGamerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/retrogamer/RetroGamerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.retrogamer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetroGamerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
