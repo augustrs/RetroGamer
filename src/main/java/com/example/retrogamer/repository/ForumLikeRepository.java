4ad609868ea46a7f89489f40535f54b9423bb4ce
Thomas <thni0004@stud.kea.dk>
2024-11-28 13:49:15 +0100

    ForumLikeRepository

---
+++ src/main/java/com/example/retrogamer/repository/ForumLikeRepository.java
@@ -0,0 +1,8 @@
+package com.example.retrogamer.repository;
+
+import com.example.retrogamer.model.ForumLike;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ForumLikeRepository extends JpaRepository<ForumLike, Long> {
+
+}
